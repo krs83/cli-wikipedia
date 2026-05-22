f5c4c7d9497eaccfc494919b9730bd61897a63b5
krs83 <krs83@yandex.ru>
2026-05-22 11:36:35 +0300

    refactor: add command_runner library

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,58 +1,8 @@
-import 'package:cli/cli.dart' as cli;
-import 'dart:io';
-import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  print('Hello world: ${cli.calculate()}!');
-  const version = '0.0.1';
-
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage();
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'wikipedia') {
-    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
-  } else {
-    printUsage();
-  }
-}
-
-void searchWikipedia(List<String>? arguments) async {
-  final String articleTitle;
-
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title.');
-
-    final inputFromStdin = stdin.readLineSync();
-    if (inputFromStdin == null || inputFromStdin.isEmpty) {
-      print('No article title provided. Exiting.');
-      return;
-    }
-    articleTitle = inputFromStdin;
-  } else {
-    articleTitle = arguments.join(' ');
-  }
-
-  print('Looking up articles about "$articleTitle". Please wait.');
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent);
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org',
-    'api/rest_v1/page/summary/$articleTitle',
-  );
-  final response = await http.get(url);
-  if (response.statusCode == 200) {
-    return response.body;
-  }
-  return 'Error: Failed to fetch article $articleTitle. Status code : ${response.statusCode}';
-}
-
-void printUsage() {
-  print(
-    "The following commands are valid: 'help', 'version', 'wikipedia <ARTICLE-TITLE>",
-  );
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
